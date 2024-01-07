b7c3f7636d3420900d06c29911be597797c87b9d
Ognjen Grgur <ognjen.grgur@continental-corporation.com>
2024-01-07 18:30:53 +0100

    Add project files.

---
+++ EvidencijaNezaposlenih.Repozitorijum/IRepozitorijum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+public interface IRepozitorijum<T> where T : class
+{
+    T DajSvePoID(object id);
+    IEnumerable<T> DajSve();
+    IEnumerable<T> Pronađi(Expression<Func<T, bool>> uslov);
+
+    void Dodaj(T entitet);
+    void DodajSve(IEnumerable<T> entiteti);
+
+    void Ukloni(T entitet);
+    void UkloniSve(IEnumerable<T> entiteti);
+}
