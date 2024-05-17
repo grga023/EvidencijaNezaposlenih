f4d3b09a1ebf083826b4a10dbf2fc2208dc10519
Ognjen Grgur <ognjen.grgur@continental-corporation.com>
2024-05-17 17:00:11 +0200

    prikaz nez

---
+++ Evidencijanezaposlenih.Interface/Pages/NezaposleniPikaz.cshtml.cs
@@ -0,0 +1,36 @@
+using EvidencijaNezaposlenih.ModeliPodataka.DTO;
+using EvidencijaNezaposlenih.Servisi.Interfejsi;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Evidencijanezaposlenih.Interface.Pages
+{
+    public class NezaposleniPrikazModel : PageModel
+    {
+        private readonly INezaposleniServis _nezaposleniService;
+
+        public NezaposleniPrikazModel(INezaposleniServis nezaposleniService)
+        {
+            _nezaposleniService = nezaposleniService;
+        }
+        public List<NezaposleniPrikaz> NezaposleniList { get; set; }
+        public async Task OnGetAsync()
+        {
+            var data = await _nezaposleniService.DajSve();
+            foreach (var item in data)
+            {
+                NezaposleniPrikaz nezaposleni = new()
+                {
+                    Ime = item.Ime,
+                    Prezime = item.Prezime,
+                    Adresa = item.Adresa,
+                    BrojTelefona = item.BrojTelefona,
+                    DatumRodjenja = item.DatumRodjenja,
+                    RadniOdnosPrikaz = item.RadniOdnosPrikaz,
+                };
+                NezaposleniList.Add(nezaposleni);
+            }
+            
+        }
+    }
+}
